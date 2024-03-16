5e2f966ad78588e8cf9859e1c615793b14f2aa44
Kirill Dvoretskov <65371121+kiruxaspb@users.noreply.github.com>
2024-03-16 12:53:24 +0300

    Add readme in pypi

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ setup(
     author='SingularityNET Foundation',
     author_email='info@singularitynet.io',
     description='SingularityNET Ecosystem Contracts',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     license='MIT',
     python_requires='>=3.10',
     install_requires=[
